8feca2bd32984eabd815705e237aef4255c675db
Matthew Rocklin <mrocklin@gmail.com>
2011-05-09 13:22:57 -0500

    added output function to hwsubmit script. Collates output from the students' programs.

--- scripts/hwSubmitManipulation.py
+++ scripts/hwSubmitManipulation.py
@@ -3,7 +3,7 @@ import os
 from glob import glob
 
 
-#dirname = lambda s: s[:4]+s[4:].split('_')[0]
+username = lambda dir: dir.split('_')[1]
 dirname = lambda s: os.path.splitext(s)[0]
 
 
@@ -33,13 +33,23 @@ def createJoinedFiles():
       pass
     myfiles = glob('HW*_%s*/*.java'%cnetid)
     myfiles.sort()
+    myfiles = [file for file in myfiles if 'Data.java' not in file]
     printfile = open('joined_%s.txt'%cnetid, 'w')
     printfile.write(joinFiles(myfiles))
     printfile.close()
 
 
 
+def unjar():
+  jarfiles = glob('*.jar')
 
+  for jfile in jarfiles:
+    dir = dirname(jfile)
+    os.mkdir(dir)
+    os.system('mv %s %s'%(jfile,dir));
+    os.chdir(dir);
+    os.system('jar xf %s'%jfile);
+    os.chdir('..');
 
 def go():
   jarfiles = glob('*.jar')
@@ -51,9 +61,18 @@ def go():
     os.chdir(dir);
     os.system('jar xf %s'%jfile);
     os.chdir('..');
-  txtfiles = [f for f in glob('*.txt') if len(f.split('_'))>2]
-  txtfiles.sort()
-  for txt in txtfiles:
-    os.system('enscript -r2 -P ry160b-2 '+txt)
+#  txtfiles = [f for f in glob('*.txt') if len(f.split('_'))>2]
+#  txtfiles.sort()
+#  for txt in txtfiles:
+#    os.system('enscript -r2 -P ry160b-2 '+txt)
 
+def output():
+  dirs = [x for x in sorted(glob('HW5_*')) if '.txt' not in x and '.html' not in x]
+  homeworkdir = '/home/mrocklin/Downloads/hw5/'
+  for dir in dirs:
+    os.chdir(homeworkdir+dir)
+    pipe = os.popen('java Data')
+    f = open(homeworkdir+'output_'+username(dir)+'.txt', 'w')
+    f.write(pipe.read())
+    f.close();
 
